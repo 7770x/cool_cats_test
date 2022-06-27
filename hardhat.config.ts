baea663d490bcc792e52d2603b3e8b828f92b01b
7770x <777123777@tutanota.com>
2022-06-27 19:54:35 +0100

    added hh compiler

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,7 +15,7 @@ const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   solidity: {
-    compilers: [{ version: "0.8.0", settings: {} }],
+    compilers: [{ version: "0.8.1", settings: {} }],
   },
   networks: {
     hardhat: {},
